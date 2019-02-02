25e73dc48fb7d6945cf40dd0ca49f4ef9bd5b5f7
ninjin <fumiya8811@gmail.com>
2019-02-02 14:55:44 +0900

    Initial Commit

---
+++ Hi-Touch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hi-Touch
+//
+//  Created by 吉野史也 on 2019/02/02.
+//  Copyright © 2019年 yoshinofumiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
