ab89e9ca84c49236d4f9a6ad55c77169d8138a6e
ninjin <fumiya8811@gmail.com>
2019-02-02 23:07:53 +0900

    Completed setting Register and Login ver.1

--- Hi-Touch/Controller/LoginViewController.swift
+++ Hi-Touch/Controller/LoginViewController.swift
@@ -7,29 +7,43 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func LoginButtonPressed(_ sender: UIButton) {
+        SVProgressHUD.show()
+        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil{
+                SVProgressHUD.dismiss()
+                let alert = UIAlertController(title: "エラー", message: "メールアドレスかパスワードが有効ではありません", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+                
+                print("Could not create a user: \(String(describing: error))")
+            }else{
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToMain", sender: self)
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+        
     }
-    */
-
+    
 }
