38e22b5c979688510bcc7320fc4e5359c5d9fbf3
ninjin <fumiya8811@gmail.com>
2019-02-03 19:07:11 +0900

    Created Profile Cell

---
+++ Hi-Touch/Custom Cell/ProfileCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileCell.swift
+//  Hi-Touch
+//
+//  Created by 吉野史也 on 2019/02/03.
+//  Copyright © 2019年 yoshinofumiya. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var teamLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+
+    }
+
+}
