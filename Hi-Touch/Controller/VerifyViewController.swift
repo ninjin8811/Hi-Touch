f0f267dd5f98a5c26973da16d42bfdbfa0fb51e4
ninjin <fumiya8811@gmail.com>
2019-02-03 13:52:22 +0900

    putting verifying phase aside for now

--- Hi-Touch/Controller/VerifyViewController.swift
+++ Hi-Touch/Controller/VerifyViewController.swift
@@ -8,24 +8,42 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class VerifyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func verifyButtonPressed(_ sender: UIButton) {
-        
-        let alert = UIAlertController(title: "エラー", message: "認証が確認できません", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
+//        SVProgressHUD.show()
+//
+//        if Auth.auth().currentUser?.isEmailVerified == true{
+//            SVProgressHUD.dismiss()
+//
+//            performSegue(withIdentifier: "goToMain", sender: self)
+//            dismiss(animated: true, completion: nil)
+//        }else{
+//            SVProgressHUD.dismiss()
+//
+//            let alert = UIAlertController(title: "エラー", message: "認証が確認できません", preferredStyle: .alert)
+//            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//
+//            alert.addAction(okAction)
+//            present(alert, animated: true, completion: nil)
+//        }
+        dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: "goToMain", sender: self)
     }
     
     @IBAction func sendButtonPressed(_ sender: UIButton) {
+        Auth.auth().currentUser?.sendEmailVerification(completion: { (error) in
+            if error != nil{
+                print("Could not send Email")
+            }else{
+                print("Sent Message!!")
+            }
+        })
     }
-    
 }
