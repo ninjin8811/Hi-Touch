2950c3201487e03d84efda6b0532626bb0d5f079
ninjin <fumiya8811@gmail.com>
2019-02-06 17:07:05 +0900

    storyboard var5 ????

---
+++ Hi-Touch/Controller/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  Hi-Touch
+//
+//  Created by 吉野史也 on 2019/02/06.
+//  Copyright © 2019年 yoshinofumiya. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var teamTextField: UITextField!
+    @IBOutlet weak var regionTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+    }
+}
