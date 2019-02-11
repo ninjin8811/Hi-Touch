54c82001179b0cec57927dea089dec146d0a130c
ninjin <fumiya8811@gmail.com>
2019-02-12 03:57:46 +0900

    bafore setting Chat

--- Hi-Touch/Controller/SearchViewController.swift
+++ Hi-Touch/Controller/SearchViewController.swift
@@ -9,31 +9,53 @@
 import UIKit
 import Firebase
 
-class SearchViewController: UITableViewController {    
+class SearchViewController: UITableViewController {
+    
+    @IBOutlet var searchTableView: UITableView!
+    
+    
+    var searchedData = [Profile](){
+        didSet{
+            loadItems()
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-
     }
     
+    @IBAction func check(_ sender: UIBarButtonItem) {
+            tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        searchTableView.register(UINib(nibName: "ProfileCell", bundle: nil), forCellReuseIdentifier: "profileCell")
+    }
+    
+    func loadItems(){
+        tableView.reloadData()
+    }
     
 
     // MARK: - Table view delegate
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
+        return searchedData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath) as! ProfileCell
+        
+        cell.ageLabel.text = searchedData[indexPath.row].age
+        cell.nameLabel.text = searchedData[indexPath.row].name
+        cell.regionLabel.text = searchedData[indexPath.row].region
+        cell.teamLabel.text = searchedData[indexPath.row].team
+        
+        return cell
     }
 
 }
--- Hi-Touch/Controller/SetTermsViewController.swift
+++ Hi-Touch/Controller/SetTermsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SetTermsViewController: UIViewController {
     
@@ -15,20 +16,108 @@ class SetTermsViewController: UIViewController {
     @IBOutlet weak var teamTextField: UITextField!
     @IBOutlet weak var regionTextField: UITextField!
     
-    
+    let dataRef = Database.database().reference().child("users")
+    var list = [Profile]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    
+/*-----------------------------------------------------------------------------------------*/
+    //Search User Methods
+    
+    func searchFirebaseDatabase(child: String, equelValue: String){
+        
+        let ref = dataRef.queryOrdered(byChild: child).queryEqual(toValue: equelValue)
+        
+        ref.observeSingleEvent(of: DataEventType.value, with: { (snapshot) in
 
+            for item in snapshot.children{
+                let snap = item as! DataSnapshot
+                let snapshotValue = snap.value as! [String: String]
+                
+                self.narrowSearchedData(snapshotValue)
+            }
+            self.goToPreviousView()
+        })
     }
     
     
     
     
+    func narrowSearchedData(_ value: Dictionary<String, String>){
+        
+        if genderTextField.text! != ""{
+            if genderTextField.text! != value["gender"]{
+                return
+            }
+        }
+        if ageTextField.text! != ""{
+            if ageTextField.text! != value["age"]{
+                return
+            }
+        }
+        if teamTextField.text! != ""{
+            if teamTextField.text! != value["team"]{
+                return
+            }
+        }
+        if regionTextField.text! != ""{
+            if teamTextField.text! != value["team"]{
+                return
+            }
+        }
+        
+        let addUser = Profile()
+        addUser.age = value["age"]!
+        addUser.name = value["name"]!
+        addUser.region = value["region"]!
+        addUser.team = value["team"]!
+        addUser.userID = value["userID"]!
+        
+        list.append(addUser)
+    }
     
+    
+/*-----------------------------------------------------------------------------------------*/
+    //MARK: - View Will Desappear
+    
+    func goToPreviousView(){
+        let nav = self.navigationController
+        let nextVC = nav?.viewControllers[(nav?.viewControllers.count)! - 2] as! SearchViewController
+
+        nextVC.searchedData = list
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
+/*-----------------------------------------------------------------------------------------*/
+    //MARK: - Button Action
     
     
     @IBAction func searchButtonPressed(_ sender: UIButton) {
+
+        list.removeAll()
+
+        DispatchQueue.main.async {
+            if self.teamTextField.text! != "" {
+                self.searchFirebaseDatabase(child: "team", equelValue: self.teamTextField.text!)
+                
+            }else if self.regionTextField.text != ""{
+                self.searchFirebaseDatabase(child: "region", equelValue: self.regionTextField.text!)
+                
+            }else if self.ageTextField.text != ""{
+                self.searchFirebaseDatabase(child: "age", equelValue: self.ageTextField.text!)
+                
+            }else if self.genderTextField.text != ""{
+                
+            }
+        }
     }
-    
 }
+
+
